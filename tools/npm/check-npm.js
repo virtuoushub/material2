ce4910fd00998d38b55c489e3313c47f5aeb3d74
Peter Colapietro <pcolapietro@greencheckverified.com>
2018-11-26 11:45:01 -0500

    For testing

--- tools/npm/check-npm.js
+++ tools/npm/check-npm.js
@@ -6,8 +6,8 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-if (process.env.npm_execpath.indexOf('yarn') === -1) {
-    console.error('Please use Yarn instead of NPM to install dependencies. ' +
-                  'See: https://yarnpkg.com/lang/en/docs/install/');
-    process.exit(1);
-}
+// if (process.env.npm_execpath.indexOf('yarn') === -1) {
+//     console.error('Please use Yarn instead of NPM to install dependencies. ' +
+//                   'See: https://yarnpkg.com/lang/en/docs/install/');
+//     process.exit(1);
+// }
